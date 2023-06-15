73021473904baec054f55db9d8765d9f59b42815
ferpmdev <ferpm.dev@gmail.com>
2023-06-15 20:01:07 -0300

    agrega boton de retroceso

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,10 +1,12 @@
 import Hotels from '../hoteles.json';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export const Details = () => {
   const params = useParams();
   console.log(params);
   const hotel = Hotels[params.id - 1];
+
+  const navigate = useNavigate();
 
   return (
     <div className="card" style={{ width: '18rem' }}>
@@ -12,6 +14,7 @@ export const Details = () => {
       <div class="card-body">
         <h5 class="card-title">{hotel.name}</h5>
         <p class="card-text">{hotel.description}</p>
+        <button onClick={() => navigate(-1)}>↩Back</button>
       </div>
     </div>
   );
